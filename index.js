1bd64f9dc2c32938f5e247c01b3b5616fdab19b6
AlexLaval <alex.laval.g@gmail.com>
2022-07-08 15:48:38 +0200

    🎨(project) format code

--- index.js
+++ index.js
@@ -21,35 +21,43 @@ bot.onText(/\/help/, (msg) =>
 );
 bot.onText(/\/start|\/game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
-    if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
-    } else {
-        queries[query.id] = query;
+  if (query.game_short_name !== gameName) {
+    bot.answerCallbackQuery(
+      query.id,
+      "Sorry, '" + query.game_short_name + "' is not available."
+    );
+  } else {
+    queries[query.id] = query;
     const gameurl = `https://${webURL}/index.html?id=${query.id}`;
     bot.answerCallbackQuery(query.id, { url: gameurl });
-    }
+  }
 });
-bot.on("inline_query", function(iq) {
-      bot.answerInlineQuery(iq.id, [ { type: "game", id: "0", game_short_name: gameName } ] ); 
+bot.on("inline_query", function (iq) {
+  bot.answerInlineQuery(iq.id, [
+    { type: "game", id: "0", game_short_name: gameName },
+  ]);
 });
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 
-server.get("/highscore/:score", function(req, res, next) {
-    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
+server.get("/highscore/:score", function (req, res, next) {
+  if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
+
   const query = queries[req.query.id];
   const score = parseInt(req.params.score);
-    let options;
-    if (query.message) {
-        options = {
-            chat_id: query.message.chat.id,
-            message_id: query.message.message_id
-        };
-    } else {
-        options = {
-            inline_message_id: query.inline_message_id
-        };
-    }
+
+  let options;
+  if (query.message) {
+    options = {
+      chat_id: query.message.chat.id,
+      message_id: query.message.message_id,
+    };
+  } else {
+    options = {
+      inline_message_id: query.inline_message_id,
+    };
+  }
+
   bot
     .setGameScore(query.from.id, score, options)
     .then((b) => {
