213c313c4f29b645d8fc2f9d27c207b732ea9ea3
AlexLaval <alex.laval.g@gmail.com>
2022-07-08 15:44:27 +0200

    👽️(botapi) modify deprecated command
    answerCallbackQuery([object]) has been deprecated.
    Updated with correct implementation.

--- index.js
+++ index.js
@@ -23,11 +23,8 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://trexgame.herokuapp.com/index.html?id="+query.id;
-        bot.answerCallbackQuery({
-            callback_query_id: query.id,
-            url: gameurl
-        });
+    const gameurl = "https://trexgame.herokuapp.com/index.html?id=" + query.id;
+    bot.answerCallbackQuery(query.id, { url: gameurl });
     }
 });
 bot.on("inline_query", function(iq) {
